a257c6598427415e1961b5c285b4d4f759500c75
BioHazard <3482991796@qq.com>
2025-08-09 13:19:00 +0800

    feat: 在任意寻访中获得若干名任意 6★ 干员、5★ 及以上干员、5★ 干员所需抽数的分布

--- plot_tools.py
+++ plot_tools.py
@@ -7,10 +7,12 @@ from matplotlib import cm
 from matplotlib.ticker import PercentFormatter
 from scipy import stats
 
+# 网格线1 < 水印2 < 填充3 < 横线4 < 竖线5 < 曲线6 < 标记点7 < 右侧文字8 < 分位数文字9 < 期望文字10 < 众数文字11
+
 # matplotlib 绘图设置
 plt.rcParams["font.sans-serif"] = ["Source Han Sans SC"]
 # plt.rcParams["figure.figsize"] = (10.0, 8.0)
-plt.rcParams["figure.dpi"] = 72
+plt.rcParams["figure.dpi"] = 300
 # # 默认添加 minor tick
 # plt.rcParams["xtick.minor.visible"] = True
 # plt.rcParams["ytick.minor.visible"] = True
@@ -45,48 +47,48 @@ def draw_pmf_cdf_fig(dist: FiniteDist,
 
     # 绘制 PMF 和 CDF，并在曲线下方填充颜色
     ax1.fill_between(x, dist.pk, step="mid", alpha=0.3, zorder=3)
-    ax1.plot(x, dist.pk, drawstyle=drawstyle, label="概率质量函数", path_effects=stroke_white, zorder=10)
+    ax1.plot(x, dist.pk, drawstyle=drawstyle, path_effects=stroke_white, zorder=6)
     ax2.fill_between(x, dist.cdf(x), step="mid", alpha=0.3, zorder=3)
-    ax2.plot(x, dist.cdf(x), drawstyle=drawstyle, label="累积分布函数", path_effects=stroke_white, zorder=10)
+    ax2.plot(x, dist.cdf(x), drawstyle=drawstyle, path_effects=stroke_white, zorder=6)
 
     # 期望值
-    ax1.axvline(dist.expect(), color="C1", linestyle="--", label="期望值")
+    ax1.axvline(dist.expect(), linestyle="--", color="C1", zorder=5)
     ax1.annotate(f"期望\n{dist.expect():.2f}抽", (dist.expect(), ax1_y_top),
                  ha="center", va="top", xytext=(0, -5), textcoords="offset points",
-                 color="C1", fontweight="medium", path_effects=stroke_white)
+                 color="C1", fontweight="medium", path_effects=stroke_white, zorder=10)
 
     # 分位数
     [
-        ax1.axvline(quantile_point, color="gray", linestyle="--")
+        ax1.axvline(quantile_point, color="gray", linestyle="--", zorder=6)
         for quantile_point in quantile_points
     ]
     [
         ax1.annotate(f"{quantile_point}抽\n{dist.cdf(quantile_point):.0%}", (quantile_point, ax1_y_top),
                      ha="center", va="top", xytext=(0, -5), textcoords="offset points",
-                     color="gray", fontweight="medium", path_effects=stroke_white)
+                     color="gray", fontweight="medium", path_effects=stroke_white, zorder=9)
         for quantile_point in quantile_points
     ]
 
     # 众数
     ax1.scatter(np.argmax(dist.pk), np.max(dist.pk),
-                color="C1", marker=".", path_effects=stroke_white, zorder=20)
+                color="C1", marker=".", path_effects=stroke_white, zorder=7)
     ax1.annotate(f"最有可能\n{np.argmax(dist.pk)}抽", (np.argmax(dist.pk), np.max(dist.pk)),  # type: ignore
                  ha="center", va="bottom", xytext=(0, 5), textcoords="offset points",
-                 color="C1", fontweight="medium", path_effects=stroke_white)
+                 color="C1", fontweight="medium", path_effects=stroke_white, zorder=11)
 
     # 期望值
-    ax2.axvline(dist.expect(), color="C1", linestyle="--", label="期望值")
+    ax2.axvline(dist.expect(), linestyle="--", color="C1", zorder=5)
     ax2.annotate(f"期望\n{dist.expect():.2f}抽", (dist.expect(), ax2_y_top),
                  ha="center", va="top", xytext=(0, -5), textcoords="offset points",
-                 color="C1", fontweight="medium", path_effects=stroke_white)
+                 color="C1", fontweight="medium", path_effects=stroke_white, zorder=10)
 
     # 分位数
     ax2.scatter(quantile_points, dist.cdf(quantile_points),
-                color="C0", marker=".", path_effects=stroke_white, zorder=20)
+                color="C0", marker=".", path_effects=stroke_white, zorder=7)
     [
         ax2.annotate(f"{quantile_point}抽\n{dist.cdf(quantile_point):.0%}", (quantile_point, dist.cdf(quantile_point)),  # type: ignore
                      ha="right", va="baseline", xytext=(-5, 5), textcoords="offset points",
-                     color="gray", fontweight="medium", path_effects=stroke_white)
+                     color="gray", fontweight="medium", path_effects=stroke_white, zorder=9)
         for quantile_point in quantile_points
     ]
 
@@ -96,7 +98,7 @@ def draw_pmf_cdf_fig(dist: FiniteDist,
     ax2.set_title("累积分布函数", fontweight="bold")
     ax1.set_ylabel("本抽概率", fontweight="bold")
     ax2.set_ylabel("累积概率", fontweight="bold")
-    ax2.set_xlabel("抽数", fontweight="bold")
+    ax2.set_xlabel("寻访次数", fontweight="bold")
 
     for ax in (ax1, ax2):
         # 绘制水印
@@ -131,32 +133,29 @@ def draw_multi_cdf_fig(dists: list[FiniteDist],
     fig = plt.figure(figsize=(10, 6), layout="tight")
     ax: plt.Axes = fig.subplots(1, 1)  # type: ignore
     colors = [f"C{i}" for i in range(len(dists))]
-    ax_y_top = 1.2
+    ax_y_top = 1.15
 
     for dist, label, color in zip(dists, labels, colors):
         x = np.arange(len(dist.pk))
-        ax.plot(x, dist.cdf(x), color=color, drawstyle=drawstyle, path_effects=stroke_white, label=label, zorder=10)
+        ax.plot(x, dist.cdf(x), color=color, drawstyle=drawstyle, path_effects=stroke_white, label=label, zorder=6)
 
-    # 期望值
-    for dist, color in zip(dists, colors):
-        ax.axvline(dist.expect(), color=color, linestyle="--")
+        # 期望值
+        ax.axvline(dist.expect(), linestyle="--", color=color, zorder=5)
         ax.annotate(f"期望\n{dist.expect():.2f}", (dist.expect(), ax_y_top),
                     ha="center", va="top", xytext=(0, -5), textcoords="offset points",
-                    color=color, fontweight="medium", path_effects=stroke_white)
+                    color=color, fontweight="medium", path_effects=stroke_white, zorder=10)
         # ax.annotate(f"{dist.expect():.2f}", (round(dist.expect()), dist.cdf(round(dist.expect()))),
         #             ha="center", va="baseline",
         #             color=color, fontweight="medium", path_effects=stroke_white, zorder=20)
 
-    # 分位数
-    for i, dist in enumerate(dists):
-        x = np.arange(len(dist.pk))
+        # 分位数
         quantile_points = calc_quantile_point(dist.cdf(x), quantile_poses)
         ax.scatter(quantile_points, dist.cdf(quantile_points),
-                   s=10, color=f"C{i}", marker=".", path_effects=stroke_white, zorder=20)
+                   s=10, color=color, marker=".", path_effects=stroke_white, zorder=7)
         [
             ax.annotate(quantile_point, (quantile_point, quantile_pos),
                         ha="right", va="baseline", xytext=(-2, 2), textcoords="offset points",
-                        color="gray", fontweight="medium", path_effects=stroke_white, zorder=20)
+                        color="gray", fontweight="medium", path_effects=stroke_white, zorder=9)
             for quantile_point, quantile_pos in zip(quantile_points, quantile_poses)
         ]
         [
@@ -166,15 +165,15 @@ def draw_multi_cdf_fig(dists: list[FiniteDist],
         [
             ax.annotate(f"{quantile_pos:.0%}", (x_max, quantile_pos),
                         ha="right", va="center", xytext=(-5, 0), textcoords="offset points",
-                        color="gray", fontweight="medium", path_effects=stroke_white, zorder=20)
+                        color="gray", fontweight="medium", path_effects=stroke_white, zorder=8)
             for quantile_pos in quantile_poses
         ]
 
-    # 图表标题，加粗
-    fig.suptitle(f"明日方舟终末地「再次测试」@罗德岛基建BETA\n{title}", fontweight="bold", fontsize="x-large")
+    # 图表标题
+    fig.suptitle(f"明日方舟寻访机制解析　bilibili@Bio-Hazard\n{title}", fontweight="bold", fontsize="x-large")
     ax.set_title("累积分布函数", fontweight="bold")
     ax.set_ylabel("累积概率", fontweight="bold")
-    ax.set_xlabel("抽数", fontweight="bold")
+    ax.set_xlabel("寻访次数", fontweight="bold")
 
     # 启用 minor tick
     ax.minorticks_on()
@@ -196,3 +195,102 @@ def draw_multi_cdf_fig(dists: list[FiniteDist],
     fig.savefig(f"图片/{title}.png", dpi=300)
 
     return fig, (ax,)
+
+
+def draw_multi_pmf_cdf_fig(dists: list[FiniteDist],
+                           labels: list[str],
+                           title: str,
+                           quantile_poses,
+                           *,
+                           x_max: float,
+                           ax1_y_top: float | None = None,
+                           drawstyle=None):
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10), layout="tight")
+    ax1: plt.Axes  # type: ignore
+    ax2: plt.Axes  # type: ignore
+    colors = [f"C{i}" for i in range(len(dists))]
+    if ax1_y_top is None:
+        ax1_y_top = np.max(dists[0].pk) * 1.27
+    ax2_y_top = 1.2
+
+    for dist, label, color in zip(dists, labels, colors):
+        x = np.arange(len(dist.pk))
+        ax1.fill_between(x, dist.pk, step="mid", alpha=0.3, zorder=3)
+        ax1.plot(x, dist.pk, label=label,
+                 color=color, drawstyle=drawstyle, path_effects=stroke_white, zorder=6)
+        # ax2.fill_between(x, dist.cdf(x), step="mid", alpha=0.3, zorder=3)
+        ax2.plot(x, dist.cdf(x), label=label,
+                 color=color, drawstyle=drawstyle, path_effects=stroke_white, zorder=6)
+
+        # 期望值
+        ax1.axvline(dist.expect(), linestyle="--", color=color, zorder=5)  # type: ignore
+        ax1.annotate(f"期望\n{dist.expect():.2f}", (dist.expect(), ax1_y_top),  # type: ignore
+                     ha="center", va="top", xytext=(0, -5), textcoords="offset points",
+                     color=color, fontweight="medium", path_effects=stroke_white, zorder=10)
+        ax2.axvline(dist.expect(), linestyle="--", color=color, zorder=5)  # type: ignore
+        ax2.annotate(f"期望\n{dist.expect():.2f}", (dist.expect(), ax2_y_top),  # type: ignore
+                     ha="center", va="top", xytext=(0, -5), textcoords="offset points",
+                     color=color, fontweight="medium", path_effects=stroke_white, zorder=10)
+
+        # 众数
+        ax1.scatter(np.argmax(dist.pk), np.max(dist.pk),
+                    s=10, color=color, marker=".", path_effects=stroke_white, zorder=7)
+        ax1.annotate(f"最有可能\n{np.argmax(dist.pk)}", (np.argmax(dist.pk), np.max(dist.pk)),  # type: ignore
+                     ha="center", va="bottom", xytext=(0, 5), textcoords="offset points",
+                     color=color, fontweight="medium", path_effects=stroke_white, zorder=11)
+
+        # 分位数
+        quantile_points = calc_quantile_point(dist.cdf(x), quantile_poses)
+        ax2.scatter(quantile_points, dist.cdf(quantile_points),
+                    s=10, color=color, marker=".", path_effects=stroke_white, zorder=7)
+        [
+            ax2.annotate(quantile_point, (quantile_point, quantile_pos),
+                         ha="right", va="baseline", xytext=(-2, 2), textcoords="offset points",
+                         color="gray", fontweight="medium", path_effects=stroke_white, zorder=9)
+            for quantile_point, quantile_pos in zip(quantile_points, quantile_poses)
+        ]
+        [
+            ax2.axhline(quantile_pos, color="gray", linestyle="--", zorder=4)
+            for quantile_pos in quantile_poses
+        ]
+        [
+            ax2.annotate(f"{quantile_pos:.0%}", (x_max, quantile_pos),
+                         ha="right", va="center", xytext=(-5, 0), textcoords="offset points",
+                         color="gray", fontweight="medium", path_effects=stroke_white, zorder=8)
+            for quantile_pos in quantile_poses
+        ]
+
+    # 图表标题
+    fig.suptitle(f"明日方舟寻访机制解析　bilibili@Bio-Hazard\n{title}", fontweight="bold", fontsize="x-large")
+    ax1.set_title("概率质量函数", fontweight="bold")
+    ax2.set_title("累积分布函数", fontweight="bold")
+    ax1.set_ylabel("本抽概率", fontweight="bold")
+    ax2.set_ylabel("累积概率", fontweight="bold")
+    ax2.set_xlabel("寻访次数", fontweight="bold")
+
+    for ax in (ax1, ax2):
+        # 绘制水印
+        ax.text(0.98, 0.02, "bilibili@Bio-Hazard", transform=ax.transAxes, ha="right", va="bottom",
+                fontsize=14, fontweight="medium", color="gray", path_effects=stroke_white, zorder=2)
+
+        # 显示网格
+        ax.minorticks_on()
+        ax.grid(True, which="major", linewidth=1.2)
+        ax.grid(True, which="minor", linewidth=0.6)
+
+        # 坐标轴范围
+        ax.set_xlim(0, x_max)
+
+        # y 轴标签显示为百分比
+        ax.yaxis.set_major_formatter(PercentFormatter(1))
+
+    # 坐标轴范围
+    ax1.set_ylim(0, ax1_y_top)
+    ax2.set_ylim(0, ax2_y_top)
+
+    # 添加图例
+    ax1.legend()
+
+    fig.savefig(f"图片/{title}.png", dpi=300)
+
+    return fig, (ax1, ax2)
